d915ae9ec8ee02c70c85b562eb7445e58c519b37
Erno Puustinen <erno@fns.fi>
2023-03-29 17:26:07 +0300

    Fix unformatted dates problem

--- resources/js/components/ClientShow.vue
+++ resources/js/components/ClientShow.vue
@@ -50,7 +50,7 @@
                             </thead>
                             <tbody>
                                 <tr v-for="booking in client.bookings" :key="booking.id">
-                                    <td>{{ booking.start }} - {{ booking.end }}</td>
+                                    <td>{{ booking | getBookingDisplayTime }}</td>
                                     <td>{{ booking.notes }}</td>
                                     <td>
                                         <button class="btn btn-danger btn-sm" @click="deleteBooking(booking)">Delete</button>
@@ -79,18 +79,16 @@
 
 <script>
 import axios from 'axios';
+import moment from 'moment';
 
 export default {
     name: 'ClientShow',
-
     props: ['client'],
-
     data() {
         return {
             currentTab: 'bookings',
         }
     },
-
     methods: {
         switchTab(newTab) {
             this.currentTab = newTab;
@@ -98,6 +96,14 @@ export default {
 
         deleteBooking(booking) {
             axios.delete(`/bookings/${booking.id}`);
+        }
+    },
+    filters: {
+        getBookingDisplayTime: function(booking) {
+            let start = moment(booking.start)
+                .format('dddd D MMMM YYYY, H:mm');
+            let end = moment(booking.end).format('H:mm')
+            return start.concat(' to ', end)
         }
     }
 }
