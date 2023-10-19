16646f26a0053d76e457a770a7abfd7f55239971
Maciej Owcarz <maciejowcarz@Maciejs-MacBook-Pro.local>
2023-10-19 13:44:15 +0200

    fixed seeder to work with created user;

--- database/seeds/ClientSeeder.php
+++ database/seeds/ClientSeeder.php
@@ -13,10 +13,19 @@ class ClientSeeder extends Seeder
      */
     public function run()
     {
-        $user = User::first() ?? factory(User::class)->create();
+//        $user = User::first() ?? factory(User::class)->create();
+//
+//        factory(Client::class, 50)->create([
+//            'user_id' => $user->id
+//        ]);
 
-        factory(Client::class, 150)->create([
-            'user_id' => $user->id,
-        ]);
+        factory(User::class)->create();
+
+        $users = User::all();
+        foreach ($users as $user) {
+            factory(Client::class, 5)->create([
+                'user_id' => $user->id
+            ]);
+        }
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        factory(User::class)->create();
+
         $this->call(ClientSeeder::class);
         $this->call(BookingSeeder::class);
         $this->call(JournalSeeder::class);
