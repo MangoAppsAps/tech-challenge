71ce4b761382cca539e07e8ce5384d317c1bbfe9
Brad Ahrens <bradleyjamesahrens@gmail.com>
2023-10-27 11:38:42 +0100

    chore: clean up web routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\JournalController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,13 +21,11 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['middleware' => 'auth', 'prefix' => 'clients'], function () {
-    // TODO: Turn this into a resource route
-    Route::get('/', 'ClientsController@index')->name('clients.index');
-    Route::get('/create', 'ClientsController@create');
-    Route::post('/', 'ClientsController@store');
-    Route::get('/{client}', 'ClientsController@show');
-    Route::delete('/{client}', 'ClientsController@destroy');
+// TODO: Put these in API routes
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('clients', 'ClientController')->only(['index', 'create', 'store', 'show', 'destroy'])->names('clients');
 
-    Route::resource('{client}/journal', JournalController::class)->only(['store', 'destroy']);
+    Route::group(['prefix' => 'clients'], function () {
+        Route::resource('{client}/journal', 'JournalController')->only(['store', 'destroy']);
+    });
 });
