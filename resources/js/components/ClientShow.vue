5ea05a3ef8a1da959627d1a923c941d60c02fcd1
Josip <josip.opacic@gmail.com>
2023-12-21 16:45:45 +0100

    Install `moment.js` and use it to format booking display time

--- resources/js/components/ClientShow.vue
+++ resources/js/components/ClientShow.vue
@@ -50,7 +50,7 @@
                             </thead>
                             <tbody>
                                 <tr v-for="booking in client.bookings" :key="booking.id">
-                                    <td>{{ booking.start }} - {{ booking.end }}</td>
+                                    <td>{{ formatBookingTime(booking.start, booking.end) }}</td>
                                     <td>{{ booking.notes }}</td>
                                     <td>
                                         <button class="btn btn-danger btn-sm" @click="deleteBooking(booking)">Delete</button>
@@ -79,6 +79,7 @@
 
 <script>
 import axios from 'axios';
+import moment from 'moment';
 
 export default {
     name: 'ClientShow',
@@ -94,6 +95,11 @@ export default {
     methods: {
         switchTab(newTab) {
             this.currentTab = newTab;
+        },
+
+        formatBookingTime(start, end) {
+            // Format like: Monday 19 January 2020, 14:00 to 15:00
+            return moment(start).format('dddd D MMMM YYYY, HH:mm') + ' to ' + moment(end).format('HH:mm');
         },
 
         deleteBooking(booking) {
